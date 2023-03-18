3731788f7e38bc6a6b5c81edb19e393e6a2d0ffd
fzroudani <fatiroudani17@gmail.com>
2023-03-17 21:56:39 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,11 +18,11 @@ int main(void)
 	/* your code goes there */
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '8'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
 			for (k = '0'; k <= '9'; k++)
 			{
-				for (l = '1'; l <= '9'; l++)
+				for (l = '0'; l <= '9'; l++)
 				{
 					if (!(i + j < k + l))
 					{
