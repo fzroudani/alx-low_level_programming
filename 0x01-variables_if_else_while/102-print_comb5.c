72f89d37d24e8e7226c213597950b3f700ab1d6d
fzroudani <fatiroudani17@gmail.com>
2023-03-17 20:41:58 -0700

    My commit 1

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * This function prompts the user for an integer and checks if it is
+ * positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+	/* your code goes there */
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '8'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				for (l = '1'; l <= '9'; l++)
+				{
+					if (!(i + j < k + l))
+					{
+						continue;
+					}
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == '9' && j == '8')
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
